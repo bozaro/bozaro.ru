233b653483f4162613640c98e973ed2baa773944
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-04-18 07:08:43 +0300

    Update start.sh for locally testing site

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-hugo server -t beg -w
+#!/bin/bash -ex
+HUGO=0.15
+if [ ! -f .build/hugo_${HUGO}_linux_amd64/hugo_${HUGO}_linux_amd64 ]; then
+  mkdir -p .build
+  tar -xzvf .jenkins/distrib/hugo_${HUGO}_linux_amd64.tar.gz -C .build
+fi
+.build/hugo_${HUGO}_linux_amd64/hugo_${HUGO}_linux_amd64 server -t beg -w -b http://localhost/
