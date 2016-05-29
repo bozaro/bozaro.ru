0af3ebe6c0be523888c2a7224a2a85244a92ec1e
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-05-29 22:17:01 +0300

    Change theme

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -ex
-HUGO=0.15
-if [ ! -f .build/hugo_${HUGO}_linux_amd64/hugo_${HUGO}_linux_amd64 ]; then
-  mkdir -p .build
-  tar -xzvf .jenkins/distrib/hugo_${HUGO}_linux_amd64.tar.gz -C .build
+set +e
+HUGO=`which hugo`
+set -e
+if [ "${HUGO}" == "" ]; then
+  HUGO_VER=0.15
+  if [ ! -f .build/hugo_${HUGO_VER}_linux_amd64/hugo_${HUGO_VER}_linux_amd64 ]; then
+    mkdir -p .build
+    tar -xzvf .jenkins/distrib/hugo_${HUGO_VER}_linux_amd64.tar.gz -C .build
+  fi
+  HUGO=".build/hugo_${HUGO_VER}_linux_amd64/hugo_${HUGO_VER}_linux_amd64"
 fi
-.build/hugo_${HUGO}_linux_amd64/hugo_${HUGO}_linux_amd64 server -t beg -w -b http://localhost/
+${HUGO} server -t hugo-geo -w -b http://localhost/
