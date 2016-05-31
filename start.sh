9dafc9b5fa515c10a555c1df8f919272898eac40
Artem V. Navrotskiy <bozaro@yandex.ru>
2016-05-31 07:26:39 +0300

    Remove copy-paste from start.sh and Jenkinsfile

--- start.sh
+++ start.sh
@@ -1,13 +1,3 @@
 #!/bin/bash -ex
-set +e
-HUGO=`which hugo`
-set -e
-if [ "${HUGO}" == "" ]; then
-  HUGO_VER=0.15
-  if [ ! -f .build/hugo_${HUGO_VER}_linux_amd64/hugo_${HUGO_VER}_linux_amd64 ]; then
-    mkdir -p .build
-    tar -xzvf .jenkins/distrib/hugo_${HUGO_VER}_linux_amd64.tar.gz -C .build
-  fi
-  HUGO=".build/hugo_${HUGO_VER}_linux_amd64/hugo_${HUGO_VER}_linux_amd64"
-fi
-${HUGO} server -t hugo-geo -w -b http://localhost/
+cd `dirname $0`
+./build.sh server -w -b http://localhost/
