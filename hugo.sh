fc4dd664e7b8946d08d7d6a97fd5e027c85aeecd
Artem V. Navrotskiy <bozaro@yandex.ru>
2021-04-29 15:32:24 +0300

    Allow donwload hugo binary

--- hugo.sh
+++ hugo.sh
@@ -6,6 +6,7 @@ set -e
 if [ "${HUGO}" = "" ]; then
   HUGO_VER=0.82.1
   if [ ! -f .build/hugo_${HUGO_VER}_linux_amd64/hugo_${HUGO_VER}_linux_amd64 ]; then
+    mkdir -p .jenkins/distrib
     HUGO_TGZ=.jenkins/distrib/hugo_${HUGO_VER}_Linux-64bit.tar.gz
     if [ ! -f $HUGO_TGZ ]; then
       curl -fL -o ${HUGO_TGZ}~ https://github.com/gohugoio/hugo/releases/download/v${HUGO_VER}/hugo_${HUGO_VER}_Linux-64bit.tar.gz
